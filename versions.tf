a17d1370e46b0ae2612e6650529fde113aa7286f
Victor Cechinel <dev.victorcechinel@gmail.com>
2021-04-23 15:52:30 -0300

    :sparkles: Providers versions

---
+++ versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.0"
+    }
+  }
+}
