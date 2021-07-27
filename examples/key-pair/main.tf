2a8ec553d0aa27abc70a5b4f5778c830fa44d92a
Victor Cechinel <dev.victorcechinel@gmail.com>
2021-07-27 03:25:17 -0300

    :alembic: feature/1 - Update examples
    Update terraform examples

--- examples/key-pair/main.tf
+++ examples/key-pair/main.tf
@@ -1,6 +1,10 @@
 module "aws_rds_postgres" {
   source  = "victorcechinel/key-pair-ssm/aws"
-  version = "1.0.2"
+  version = "1.0.3"
 
   name = "key-pair-name"
+
+  tags = {
+    Environment = "prd"
+  }
 }
