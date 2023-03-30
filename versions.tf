7bb5081e262e3a5c23944779bc2a7d2c8b54f4b2
Victor Cechinel <dev.victorcechinel@gmail.com>
2023-03-30 13:03:59 -0300

    fix/#3 - Update terraform versions

--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.0.3"
+  required_version = ">= 1.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.37.0"
+      version = "~> 4.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 3.1.0"
+      version = "~> 4.0"
     }
   }
 }
